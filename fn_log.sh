d38d1b7e3932a03bc4e7f030f3483f424fdb92b3
Alexandre Nunes <62760370+alexandrenunesdevops@users.noreply.github.com>
2021-06-04 15:28:57 -0300

    Update fn_log.sh

--- fn_log.sh
+++ fn_log.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 [[ "$SERVICE" == "" ]] && SERVICE="script"
+
+
 LOGFL=/var/log/${SERVICE}.log
 #Function to record event logs using shell script in bash.
 #Sintaxe fnLog "<[D|I|A|E]>" "Message" "opcional N or exit code>"
@@ -13,7 +14,7 @@ fnLog(){
     case $1 in
         "D") [[ $VERBOSE -gt 2 ]] && echo -e "$DATA - $SERVICE[DEBUG] - $2" |tee -a $LOGFL;;
         "I") [[ $VERBOSE -gt 1 ]] && echo -e "$DATA - $SERVICE[INFO] - $2" |tee -a $LOGFL;;
-        "A") [[ $VERBOSE -ge 1 ]] && echo -e "$DATA - $SERVICE[WARN] - $2" |tee -a $LOGFL;;
+        "A") [[ $VERBOSE -ge 1 ]] && echo -e "$DATA - $SERVICE[WARNNING] - $2" |tee -a $LOGFL;;
         "E") echo -e "$DATA - $SERVICE[ERROR] - $2"|tee -a $LOGFL;
              [[ "${3^^}" != "N" ]] && { [[ "$3" ~= ^[0-9]+$ ]] && exit $3 || exit 1 ; };;
     esac
